06c4190a59aeaef41cec9ca3b849734496ae3e68
Jonathan Wang <jonnowangnz@gmail.com>
2022-02-02 18:28:58 +0000

    add

--- counterspeech_project-NLP/retrieval_only/train.py
+++ counterspeech_project-NLP/retrieval_only/train.py
@@ -115,6 +115,5 @@ DisplayModel.main(
     fromfile_datapath=f"{__location__}/counterspeech_project-NLP/retrieval_only/data/gab_data",
     fromfile_datatype_extension=True,
     model_file=f'{__location__}/counterspeech_project-NLP/retrieval_only/from_pretrained_retrieval/model', 
-    skip_generation=False,
     num_examples=10,
 )
