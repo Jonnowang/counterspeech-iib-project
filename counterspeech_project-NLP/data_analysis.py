30dccabe44c2c20214560b9010d313ba757ca59b
Jonathan Wang <jonnowangnz@gmail.com>
2022-02-03 15:46:04 +0000

    force fp16

--- counterspeech_project-NLP/data_analysis.py
+++ counterspeech_project-NLP/data_analysis.py
@@ -16,25 +16,27 @@ num_words=['1.', '2.', '3.', '4.', '5.']
 
 gab = pd.read_csv('counterspeech_project-NLP/data/gab.csv')
 
-# gab_words = gab['text'].str.split().map(lambda x: len(x))
-# print(gab_words.max())
-# print(gab_words.min())
-# gab_words.hist(bins=500, range=(0,1000))
-# plt.show()
+gab_words = gab['text'].str.split().map(lambda x: len(x))
+print(gab_words.max())
+print(gab_words.min())
+gab_words.hist(bins=500, range=(0,1000))
+plt.show()
 
-# reddit = pd.read_csv('counterspeech_project-NLP/data/reddit.csv')
+reddit = pd.read_csv('counterspeech_project-NLP/data/reddit.csv')
 
-# reddit_words = reddit['text'].str.split().map(lambda x: len(x))
-# print(reddit_words.max())
-# print(reddit_words.min())
-# reddit_words.hist(bins=500, range=(0,1000))
-# plt.show()
+reddit_words = reddit['text'].str.split().map(lambda x: len(x))
+print(reddit_words.max())
+print(reddit_words.min())
+reddit_words.hist(bins=500, range=(0,1000))
+plt.show()
 
-# conan = pd.read_json('counterspeech_project-NLP/data/CONAN.json')
-# print(conan.head(3))
+conan = pd.read_json('counterspeech_project-NLP/data/CONAN.json')
+print(conan.head(3))
+
+word_set = reddit['text']
 
 corpus=[]
-new= gab["text"].str.split()
+new= word_set.str.split()
 new=new.values.tolist()
 corpus=[word for i in new for word in i]
 
@@ -52,7 +54,7 @@ counter=Counter(corpus)
 most=counter.most_common()
 
 x, y= [], []
-for word,count in most[:40]:
+for word,count in most[:100]:
     if (word not in stop and word not in num_words):
         x.append(word)
         y.append(count)
@@ -69,7 +71,7 @@ def _get_top_ngram(corpus, n=None):
     words_freq =sorted(words_freq, key = lambda x: x[1], reverse=True)
     return words_freq[:50]
 
-top_n_bigrams=_get_top_ngram(gab['text'],3)[:20]
+top_n_bigrams=_get_top_ngram(word_set,2)[:20]
 x,y=map(list,zip(*top_n_bigrams))
 sns.barplot(x=y,y=x)
 plt.show()
