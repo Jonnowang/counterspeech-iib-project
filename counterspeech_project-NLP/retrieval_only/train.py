bd27e5dd70a22a70a22560469b460e53474ccf40
Jonathan Wang <jonnowangnz@gmail.com>
2022-02-10 01:11:01 +0000

    retrain retrieval

--- counterspeech_project-NLP/retrieval_only/train.py
+++ counterspeech_project-NLP/retrieval_only/train.py
@@ -1,3 +1,4 @@
+from ast import Mult
 import os
 
 from parlai.scripts.display_data import DisplayData
@@ -54,85 +55,88 @@ class MyTeacher(DialogTeacher):
         for query, response in zip(queries, responses):
             yield (query, response), True
         
-# DisplayData.main(
-#     task="fromfile:parlaiformat",
-#     fromfile_datapath=f"{__location__}/counterspeech_project-NLP/retrieval_only/data/gab_data",
-#     fromfile_datatype_extension=True,
-# )
+DisplayModel.main(
+    task="fromfile:parlaiformat",
+    fromfile_datapath=f"{__location__}/counterspeech_project-NLP/retrieval_only/data/gab_data",
+    fromfile_datatype_extension=True,
+    model_file='zoo:pretrained_transformers/poly_model_huge_reddit/model',
+    num_examples=10,
+    eval_candidates='fixed',
+    fixed_candidates_path=f"{__location__}/counterspeech_project-NLP/retrieval_only/data/counter_speech_cand.txt",
+    encode_candidate_vecs=True,
+)
 
-# DisplayModel.main(
-#     task="fromfile:parlaiformat",
-#     fromfile_datapath=f"{__location__}/counterspeech_project-NLP/retrieval_only/data/gab_data",
-#     fromfile_datatype_extension=True,
-#     model_file='zoo:pretrained_transformers/poly_model_huge_reddit/model',
-#     num_examples=10,
-#     eval_candidates='batch',
-# )
-
-# MultiProcessTrain.main(
-#     # similar to before
-#     task="fromfile:parlaiformat",
-#     fromfile_datapath=f"{__location__}/counterspeech_project-NLP/retrieval_only/data/gab_data",
-#     fromfile_datatype_extension=True,
-#     model='transformer/polyencoder',
-#     model_file=f'{__location__}/counterspeech_project-NLP/retrieval_only/from_pretrained_retrieval/model',
+MultiProcessTrain.main(
+    # similar to before
+    task="fromfile:parlaiformat",
+    fromfile_datapath=f"{__location__}/counterspeech_project-NLP/retrieval_only/data/gab_data",
+    fromfile_datatype_extension=True,
+    model='transformer/polyencoder',
+    model_file=f'{__location__}/counterspeech_project-NLP/retrieval_only/from_pretrained_retrieval/model',
+    fixed_candidates_path=f"{__location__}/counterspeech_project-NLP/retrieval_only/data/counter_speech_cand.txt",
     
-#     # initialize with a pretrained model
-#     init_model='zoo:pretrained_transformers/poly_model_huge_reddit/model',
-#     dict_file='zoo:pretrained_transformers/poly_model_huge_reddit/model.dict',
+    # initialize with a pretrained model
+    init_model='zoo:pretrained_transformers/poly_model_huge_reddit/model',
+    dict_file='zoo:pretrained_transformers/poly_model_huge_reddit/model.dict',
     
-#     # arguments we get from the pretrained model.
-#     # Unfortunately, these must be looked up separately for each model.
-#     lr_scheduler_patience=0, lr_scheduler_decay=0.4,
-#     data_parallel=True, history_size=20, label_truncate=72,
-#     text_truncate=360, veps=0.5, vme=8000,
-#     save_after_valid=True, log_every_n_secs=20, candidates='batch',
-#     dict_tokenizer='bpe', dict_lower=True,
-#     variant='xlm', reduction_type='mean', share_encoders=False,
-#     learn_positional_embeddings=True, n_layers=12, n_heads=12,
-#     ffn_size=3072, attention_dropout=0.1, relu_dropout=0.0,
-#     dropout=0.1, n_positions=1024, embedding_size=768,
-#     activation='gelu', embeddings_scale=False, n_segments=2,
-#     learn_embeddings=True, polyencoder_type='codes',
-#     poly_n_codes=64, poly_attention_type='basic',
-#     dict_endtoken='__start__', eval_candidates='batch',
+    # arguments we get from the pretrained model.
+    # Unfortunately, these must be looked up separately for each model.
+    lr_scheduler_patience=0, lr_scheduler_decay=0.4,
+    data_parallel=True, history_size=20, label_truncate=72,
+    text_truncate=360, veps=0.5, vme=8000,
+    save_after_valid=True, log_every_n_secs=20, candidates='batch',
+    dict_tokenizer='bpe', dict_lower=True,
+    variant='xlm', reduction_type='mean', share_encoders=False,
+    learn_positional_embeddings=True, n_layers=12, n_heads=12,
+    ffn_size=3072, attention_dropout=0.1, relu_dropout=0.0,
+    dropout=0.1, n_positions=1024, embedding_size=768,
+    activation='gelu', embeddings_scale=False, n_segments=2,
+    learn_embeddings=True, polyencoder_type='codes',
+    poly_n_codes=64, poly_attention_type='basic',
+    dict_endtoken='__start__', eval_candidates='fixed',
+    encode_candidate_vecs=True,
 
-#     # some training arguments, specific to this fine-tuning
-#     # use a small learning rate with ADAM optimizer
-#     lr=5e-5, optimizer='adamax',
-#     warmup_updates=100, output_scaling=0.06,
-#     # early stopping on perplexity
-#     validation_metric='accuracy',
-#     validation_metric_mode='max',
-#     # train at most 10 minutes, and validate every 0.25 epochs
-#     max_train_time=43200, validation_every_n_epochs=0.25, num_epochs=8.0,
+    # some training arguments, specific to this fine-tuning
+    # use a small learning rate with ADAM optimizer
+    lr=5e-5, optimizer='adamax',
+    warmup_updates=100, output_scaling=0.06,
+    # early stopping on perplexity
+    validation_metric='accuracy',
+    validation_metric_mode='max',
+    # train at most 10 minutes, and validate every 0.25 epochs
+    max_train_time=43200, validation_every_n_epochs=0.25, num_epochs=8.0,
     
-#     # depend on your gpu. If you have a V100, this is good
-#     batchsize=32, eval_batchsize=10, fp16=True,
-# )
+    # depend on your gpu. If you have a V100, this is good
+    batchsize=32, eval_batchsize=10, fp16=True,
+)
 
-# EvalModel.main(
-#     task="fromfile:parlaiformat",
-#     fromfile_datapath=f"{__location__}/counterspeech_project-NLP/retrieval_only/data/gab_data_test.txt",
-#     model_file='zoo:pretrained_transformers/poly_model_huge_reddit/model',
-#     eval_candidates='fixed',
-#     fixed_candidates_path=f"{__location__}/counterspeech_project-NLP/retrieval_only/data/counter_speech_cand.txt",
-# )
+MultiProcessEval.main(
+    task="fromfile:parlaiformat",
+    fromfile_datapath=f"{__location__}/counterspeech_project-NLP/retrieval_only/data/gab_data_test.txt",
+    model_file='zoo:pretrained_transformers/poly_model_huge_reddit/model',
+    eval_candidates='fixed',
+    fixed_candidates_path=f"{__location__}/counterspeech_project-NLP/retrieval_only/data/counter_speech_cand.txt",
+    encode_candidate_vecs=True,
+)
 
-# DisplayModel.main(
-#     task="fromfile:parlaiformat",
-#     fromfile_datapath=f"{__location__}/counterspeech_project-NLP/retrieval_only/data/gab_data_test.txt",
-#     model_file='zoo:pretrained_transformers/poly_model_huge_reddit/model', 
-#     num_examples=10,
-#     eval_candidates='fixed',
-#     fixed_candidates_path=f"{__location__}/counterspeech_project-NLP/retrieval_only/data/counter_speech_cand.txt",
-# )
+DisplayModel.main(
+    task="fromfile:parlaiformat",
+    fromfile_datapath=f"{__location__}/counterspeech_project-NLP/retrieval_only/data/gab_data_test.txt",
+    model_file='zoo:pretrained_transformers/poly_model_huge_reddit/model', 
+    num_examples=10,
+    eval_candidates='fixed',
+    fixed_candidates_path=f"{__location__}/counterspeech_project-NLP/retrieval_only/data/counter_speech_cand.txt",
+    encode_candidate_vecs=True,
+)
 
-# EvalModel.main(
-#     task="fromfile:parlaiformat",
-#     fromfile_datapath=f"{__location__}/counterspeech_project-NLP/retrieval_only/data/gab_data_test.txt",
-#     model_file=f'{__location__}/counterspeech_project-NLP/retrieval_only/from_pretrained_retrieval/model', 
-# )
+MultiProcessEval.main(
+    task="fromfile:parlaiformat",
+    fromfile_datapath=f"{__location__}/counterspeech_project-NLP/retrieval_only/data/gab_data_test.txt",
+    model_file=f'{__location__}/counterspeech_project-NLP/retrieval_only/from_pretrained_retrieval/model',
+    eval_candidates='fixed',
+    fixed_candidates_path=f"{__location__}/counterspeech_project-NLP/retrieval_only/data/counter_speech_cand.txt",
+    encode_candidate_vecs=True,
+)
 
 DisplayModel.main(
     task="fromfile:parlaiformat",
