ae493c86aad749df6309b95794ad9780cbfc41a3
Jonathan Wang <jonnowangnz@gmail.com>
2022-02-16 17:53:12 +0000

    run trial

--- counterspeech_project-NLP/generator_only/train.py
+++ counterspeech_project-NLP/generator_only/train.py
@@ -38,91 +38,73 @@ class MyTeacher(DialogTeacher):
         for query, response in zip(queries, responses):
             yield (query, response), True
         
-DisplayModel.main(
-    task="fromfile:parlaiformat",
-    fromfile_datapath=f"{__location__}/counterspeech_project-NLP/generator_only/data/gab_data",
-    fromfile_datatype_extension=True,
-    model_file='zoo:blender/reddit_3B/model',
-    num_examples=10,
-)
-
-# --multitask-weights 1,3,3,3 -veps 0.25 --attention-dropout 0.0
-# --model transformer/generator --embedding-size 2560 --ffn-size 10240 --variant prelayernorm 
-# --n-heads 32 --n-positions 128 --n-encoder-layers 2 --n-decoder-layers 24 --history-add-global-end-token end 
-# --delimiter '  ' --dict-tokenizer bytelevelbpe  --dropout 0.1
-# --label-truncate 128 --log_every_n_secs 10
-# --lr-scheduler reduceonplateau --lr-scheduler-patience 3 --relu-dropout 0.0 --activation gelu 
-# --model-parallel true --save-after-valid True --text-truncate 128 --truncate 128 --warmup_updates 100 
-# --fp16-impl mem_efficient --update-freq 2 --gradient-clip 0.1 --skip-generation True -vp 10 -vmt ppl -vmm min 
-# --model-file /tmp/test_train_27B
-
-TrainModel.main(
-    # similar to before
-    task="fromfile:parlaiformat",
-    fromfile_datapath=f"{__location__}/counterspeech_project-NLP/generator_only/data/gab_data",
-    fromfile_datatype_extension=True,
-    model='transformer/generator',
-    model_file=f'{__location__}/counterspeech_project-NLP/generator_only/from_pretrained_generative/model',
+# TrainModel.main(
+#     # similar to before
+#     task="fromfile:parlaiformat",
+#     fromfile_datapath=f"{__location__}/counterspeech_project-NLP/generator_only/data/gab_data",
+#     fromfile_datatype_extension=True,
+#     model='transformer/generator',
+#     model_file=f'{__location__}/counterspeech_project-NLP/generator_only/from_pretrained_generative/model',
     
-    # initialize with a pretrained model
-    init_model='zoo:blender/reddit_3B/model',
-    dict_file='zoo:blender/reddit_3B/model.dict',
+#     # initialize with a pretrained model
+#     init_model='zoo:blender/reddit_3B/model',
+#     dict_file='zoo:blender/reddit_3B/model.dict',
     
-    # arguments we get from the pretrained model.
-    # Unfortunately, these must be looked up separately for each model.
-    multitask_weights=[1,3,3,3], veps=0.25, attention_dropout=0.0,
-    embedding_size=2560, ffn_size=10240, variant='prelayernorm',
-    n_heads=32, n_positions=128, n_encoder_layers=2, n_decoder_layers=24,
-    history_add_global_end_token='end', delimiter='  ', dict_tokenizer='bytelevelbpe',
-    dropout=0.1, label_truncate=128, log_every_n_secs=10,
-    lr_scheduler="reduceonplateau", lr_scheduler_patience=3,
-    relu_dropout=0.0, activation='gelu', model_parallel=True,
-    save_after_valid=True, text_truncate=128, truncate=128,
-    update_freq=2, gradient_clip=0.1, skip_generation=True, vp=10,
-    vmt='ppl', vmm='min',
+#     # arguments we get from the pretrained model.
+#     # Unfortunately, these must be looked up separately for each model.
+#     multitask_weights=[1,3,3,3], veps=0.25, attention_dropout=0.0,
+#     embedding_size=2560, ffn_size=10240, variant='prelayernorm',
+#     n_heads=32, n_positions=128, n_encoder_layers=2, n_decoder_layers=24,
+#     history_add_global_end_token='end', delimiter='  ', dict_tokenizer='bytelevelbpe',
+#     dropout=0.1, label_truncate=128, log_every_n_secs=10,
+#     lr_scheduler="reduceonplateau", lr_scheduler_patience=3,
+#     relu_dropout=0.0, activation='gelu', model_parallel=True,
+#     save_after_valid=True, text_truncate=128, truncate=128,
+#     update_freq=2, gradient_clip=0.1, skip_generation=True, vp=10,
+#     vmt='ppl', vmm='min',
 
-    # some training arguments, specific to this fine-tuning
-    # use a small learning rate with ADAM optimizer
-    lr=7e-06, optimizer='adam',
-    warmup_updates=100,
-    # early stopping on perplexity
-    validation_metric='ppl',
-    validation_metric_mode='max',
-    # train at most 10 minutes, and validate every 0.25 epochs
-    max_train_time=43200, validation_every_n_epochs=0.25, num_epochs=8.0,
+#     # some training arguments, specific to this fine-tuning
+#     # use a small learning rate with ADAM optimizer
+#     lr=7e-06, optimizer='adam',
+#     warmup_updates=100,
+#     # early stopping on perplexity
+#     validation_metric='ppl',
+#     validation_metric_mode='max',
+#     # train at most 10 minutes, and validate every 0.25 epochs
+#     max_train_time=43200, validation_every_n_epochs=0.25, num_epochs=8.0,
     
-    # depend on your gpu. If you have a V100, this is good
-    batchsize=32, eval_batchsize=10, fp16=True,
-)
+#     # depend on your gpu. If you have a V100, this is good
+#     batchsize=32, eval_batchsize=10, fp16=True,
+# )
 
-# Evaluate Base Model
-EvalModel.main(
-    task="fromfile:parlaiformat",
-    fromfile_datapath=f"{__location__}/counterspeech_project-NLP/generator_only/data/gab_data_test.txt",
-    model_file='zoo:blender/reddit_3B/model',
-)
+# # Evaluate Base Model
+# EvalModel.main(
+#     task="fromfile:parlaiformat",
+#     fromfile_datapath=f"{__location__}/counterspeech_project-NLP/generator_only/data/gab_data_test.txt",
+#     model_file='zoo:blender/reddit_3B/model',
+# )
 
-DisplayModel.main(
-    task="fromfile:parlaiformat",
-    fromfile_datapath=f"{__location__}/counterspeech_project-NLP/generator_only/data/gab_data_test.txt",
-    force_fp16_tokens=True,
-    model_file='zoo:blender/reddit_3B/model', 
-    num_examples=10,
-    skip_generation=False,
-)
+# DisplayModel.main(
+#     task="fromfile:parlaiformat",
+#     fromfile_datapath=f"{__location__}/counterspeech_project-NLP/generator_only/data/gab_data_test.txt",
+#     force_fp16_tokens=True,
+#     model_file='zoo:blender/reddit_3B/model', 
+#     num_examples=10,
+#     skip_generation=False,
+# )
 
 # Evaluate Fine Tuned Model
 EvalModel.main(
     task="fromfile:parlaiformat",
-    fromfile_datapath=f"{__location__}/counterspeech_project-NLP/generator_only/data/gab_data_test.txt",
+    fromfile_datapath=f"{__location__}/counterspeech_project-NLP/generator_only/data/gab_trial.txt",
     model_file=f'{__location__}/counterspeech_project-NLP/generator_only/from_pretrained_generative/model', 
 )
 
 DisplayModel.main(
     task="fromfile:parlaiformat",
-    fromfile_datapath=f"{__location__}/counterspeech_project-NLP/generator_only/data/gab_data_test.txt",
+    fromfile_datapath=f"{__location__}/counterspeech_project-NLP/generator_only/data/gab_trial.txt",
     force_fp16_tokens=True,
     model_file=f'{__location__}/counterspeech_project-NLP/generator_only/from_pretrained_generative/model', 
-    num_examples=10,
+    num_examples=20,
     skip_generation=False,
 )
